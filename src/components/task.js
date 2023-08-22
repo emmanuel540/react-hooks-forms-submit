67974f5499c5aaec44676e1067083001db9ff1e2
emmanuel540! <emmanuelhillary24@gmail.com>
2023-08-23 00:37:03 +0300

    Done and dusted

---
+++ src/components/task.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Task = ({ task, onTaskDelete }) => {
+    const handleDeleteClick = () => {
+    onTaskDelete(task.id);
+    };
+    
+    return (
+    <div className="task">
+    <div>{task.text}</div>
+    <div className="label">{task.category}</div>
+    <button onClick={handleDeleteClick} className="delete">Delete</button>
+    </div>
+    );
+    };
+    
+    export default Task;
+    
